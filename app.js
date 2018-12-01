a4ff3ff0a4e42390258fa7c17f71b60ea7e371e5
Zach Wunder <zach.wunder@napv-024813mac.belkin.com>
2018-11-30 19:18:39 -0800

    first version

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
+
 
 // ROUTES:
 app.use('/', indexRouter);
--- routes/getDevices.js
+++ routes/getDevices.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const getAllDeviceInfo = require("../resources/deviceInfo").getAllDeviceInfo;
 
 router.get('/', async (req, res, next) => {
-    const devices = await getAllDeviceInfo();
-
+    //const devices = await getAllDeviceInfo();
+    const devices = [{Name: "First Device", FirmwareVersion: "V3.0.2", UDN: "8762349"},
+                      {Name: "Second Device", FirmwareVersion: "V3.0.2", UDN: "0234598"}]
     // SAVE TO CACHE
 
     res.json({
